a0402c4a29c2d20b4210b5611ba9a4d03d3715ca
Pipetboy <felipefelizx@gmail.com>
2024-03-08 21:30:58 -0300

    Feature - Footer añadido

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Header from './Components/Header';
 import CurrencyConverter from './Components/CurrencyInput';
+import Footer from './Components/Footer';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
       <CurrencyConverter />
-    </div>
-
+      <Footer />
+    </>
   );
 }
 
